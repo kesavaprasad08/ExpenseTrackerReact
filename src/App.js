12fd81237dcfb2b85196f151d991f4f2ab0e159a
kesavaprasad08 <kesav2661998@gmail.com>
2023-02-16 02:32:18 +0530

    Passing Data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import ExpenseItem from "./components/ExpenseItem";
 
+const expenses = [
+  {
+    id: "e1",
+    title: "Food",
+    Amount: 150,
+    date: new Date(2023, 2, 16),
+    locationOfExpenditure: "Chennai",
+  },
+  {
+    id: "e2",
+    title: "Petrol",
+    Amount: 100,
+    date: new Date(2021, 2, 28),
+    locationOfExpenditure: "Bangalore",
+  },
+  {
+    id: "e3",
+    title: "Movies",
+    Amount: 200,
+    date: new Date(2021, 2, 28),
+    locationOfExpenditure: "Kochi",
+  },
+  {
+    id : "e4",
+    title: "rent",
+    Amount: 20000,
+    date: new Date(2021, 1, 1),
+    locationOfExpenditure: "Chennai",
+  }
+];
 
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <h2>Expense Item</h2>
+      {expenses.map((expenses, index) => {
+        return (
+          <ExpenseItem
+            key={index}
+            title={expenses.title}
+            amount={expenses.Amount}
+            date={expenses.date}
+            location={expenses.locationOfExpenditure}
+          ></ExpenseItem>
+        );
+      })}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,49 +1,17 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate1 = new Date(2022, 3, 28);
-  const expenseDesc1 = 'Food';
-  const expensePrice1 =150;
-  const locationOfExpenditure1 = 'Chennai';
-  const expenseDate2 = new Date(2022, 4, 1);
-  const expenseDesc2 = 'Petrol';
-  const expensePrice2 =100;
-  const locationOfExpenditure2 = 'Bangalore';
-  const expenseDate3 = new Date(2022, 4, 2);
-  const expenseDesc3 = 'Movies';
-  const expensePrice3 =200;
-  const locationOfExpenditure3 = 'Kochi';
-
+function ExpenseItem(props) {
   return (
     <div>
-      <h2>Expense Item</h2>
       <div className="expense-item">
-        <div>{expenseDate1.toString()}</div>
-        <div className="expense-item__description"> <h2>{expenseDesc1} </h2>
+        <div>{props.date.toString()}</div>
+        <div className="expense-item__description">
+          <h2>{props.title} </h2>
         </div>
-        <div className="expense-item__description"> <h2>{locationOfExpenditure1} </h2>
+        <div className="expense-item__description">
+          <h2>{props.location} </h2>
         </div>
-        <div className="expense-item__price">Rs {expensePrice1}</div>
-      </div>
-
-      <div className="expense-item">
-        <div>{expenseDate2.toString()}</div>
-        <div className="expense-item__description"> <h2>{expenseDesc2} </h2>
-        </div>
-        <div className="expense-item__description"> <h2>{locationOfExpenditure2} </h2>
-        </div>
-        <div className="expense-item__price">Rs {expensePrice2}</div>
-        
-      </div>
-
-
-      <div className="expense-item">
-        <div>{expenseDate3.toString()}</div>
-        <div className="expense-item__description"> <h2>{expenseDesc3} </h2>
-        </div>
-        <div className="expense-item__description"> <h2>{locationOfExpenditure3} </h2>
-        </div>
-        <div className="expense-item__price">Rs {expensePrice3}</div>
+        <div className="expense-item__price">Rs {props.amount}</div>
       </div>
     </div>
   );
